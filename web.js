b1eb19282294f7604f76584d9d372ace3d42425b
Rob Mayette <rpmayette@gmail.com>
2013-07-14 23:34:35 +0000

    another index.html

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var FileContents = new Buffer(fs.readFileSync('index.html'));
-
+var FileContString = FileContents.ToString();
 
 app.get('/', function(request, response) {
-  response.send(FileContents.ToString());
+  response.send(FileContString);
 });
 
 var port = process.env.PORT || 5000;
