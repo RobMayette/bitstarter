093f9429ec8a5a0d5aef7b870d0a2ad65485370a
Rob Mayette <rpmayette@gmail.com>
2013-07-18 10:04:33 +0000

    Starting bootstrap

--- web.js
+++ web.js
@@ -1,22 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-
-// Read the file and print its contents.
-var fs = require('fs')
-  , filename = 'index.html';
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  FileContentsString = data;
-});
-
-
 app.get('/', function(request, response) {
-  response.send(FileContentsString);
+	var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
